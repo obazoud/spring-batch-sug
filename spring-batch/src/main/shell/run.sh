e4145613b03154ba6128271e62eeb70a2057a770
Olivier Bazoud <olivier.bazoud@gmail.com>
2010-04-18 17:53:30 +0000

    Add assembly

--- spring-batch/src/main/shell/run.sh
+++ spring-batch/src/main/shell/run.sh
@@ -4,4 +4,4 @@ set -e
 
 java -classpath $(echo ./lib/*.jar . | sed 's/ /:/g') org.springframework.batch.core.launch.support.CommandLineJobRunner \
 fr/sug/springbatch/example/batch-context.xml jobExemple \
-recipes=../../../src/test/resources/recipes.xml \
+recipes=file:///home/bazoud/java/workspace/svn/fullsix/sug/fr-sug-spring-batch/spring-batch/src/test/resources/recipes.xml \
