e50e391622ccd2450292994d698721142f77d829
Julien Jakubowski <julien.jakubowski@gmail.com>
2011-06-03 17:19:41 +0000

    comments

--- spring-batch/src/main/java/fr/sug/springbatch/example/scheduled/SchedulerMain.java
+++ spring-batch/src/main/java/fr/sug/springbatch/example/scheduled/SchedulerMain.java
@@ -3,11 +3,12 @@ package fr.sug.springbatch.example.scheduled;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Launch the job with a Quartz Scheduler.
+ * Launch the job with a Spring Scheduler.
+ * Launch with -Drecipes=URLOfBeerXmlFile
  */
 public class SchedulerMain {
     public static void main(String args[]) {
         new ClassPathXmlApplicationContext("/fr/sug/springbatch/example/scheduling-context.xml");
-        // Le scheduler va démarrer tout seul : inutile de faire plus.
+        // Spring Scheduler will start as soon as the Application Context is loaded.
     }
 }
