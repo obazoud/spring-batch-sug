c6e6d159ae7c032dbe5bdf1a67c141c65f17f09d
Olivier Bazoud <olivier.bazoud@gmail.com>
2011-06-06 09:18:44 +0000

    snippet

--- breizhjug/SkipPolicy.java
+++ breizhjug/SkipPolicy.java
@@ -1,16 +1,8 @@
 
 
-  /**
-   * Policy for determining whether or not some processing should be skipped.
-   */
   public interface SkipPolicy {
-    /**
-     * Returns true or false, indicating whether or not processing should
-     * continue with the given throwable.
-     */
     boolean shouldSkip(Throwable t, int skipCount) throws SkipLimitExceededException;
   }
-
 
   
   public interface SkipListener<T,S> extends StepListener {
@@ -22,35 +14,17 @@
 
   
   public interface JobExecutionListener {
-      /**
-       * Callback before a job executes.
-       */
       void beforeJob(JobExecution jobExecution);
-      /**
-       * Callback after completion of a job. Called after both both successful and
-       * failed executions. To perform logic on a particular status, use
-       * "if (jobExecution.getStatus() == BatchStatus.X)".
-       */
       void afterJob(JobExecution jobExecution);
   }
   
   
   public interface ItemStream {
-      /**
-       * Open the stream for the provided {@link ExecutionContext}.
-       */
       void open(ExecutionContext executionContext) throws ItemStreamException;
-      /**
-       * Indicates that the execution context provided during open is about to be saved. If any state is remaining, but
-       * has not been put in the context, it should be added here.
-       */
       void update(ExecutionContext executionContext) throws ItemStreamException;
-      /**
-       * If any resources are needed for the stream to operate they need to be destroyed here. Once this method has been
-       * called all other methods (except open) may throw an exception.
-       */
       void close() throws ItemStreamException;
   }
+  
   
   
   public class CustomItemReader<T> implements ItemReader<T>, ItemStream {
